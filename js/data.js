b3de8d7fb2b3ff6b888dff4395330e77c411a63a
JP4FF <90173422+JP4FF@users.noreply.github.com>
2023-05-12 13:42:59 +0300

    Delete data.js

--- js/data.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var reportData = getReportData();
-
-async function getReportData() {
-    return fetch('./data.json')
-        .then((response) => response.json())
-        .then((data) => data || [])
-        .catch((error) => console.log(error));
-}
